961d0112b71330fb5bdeb2ba73df416a14b2a5f6
Susana0425 <shilpakodithuwakku04@gmail.com>
2025-05-05 15:54:56 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "9K03xJAB#x2w1c8z6k3yA5SqIGLvMNMZxiR4W3q4g8U9ydohgSzs", // Add your session id
+PREFIX: process.env.PREFIX || ".",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "*_`AUTO STATUS JUST NOW SEEN BY Mitzuky MD`_*",
+MODE: process.env.MODE || "inbox",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",    
+AUTO_REACT: process.env.AUTO_REACT || "false",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",   
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
+INBOX_BLOCK: process.env.INBOX_BLOCK || "false",
+};
